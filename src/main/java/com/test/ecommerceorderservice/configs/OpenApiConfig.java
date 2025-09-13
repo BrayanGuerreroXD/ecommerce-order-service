980240b02b706ed7d609efd54aaf16b85c98a370
Brayan Guerrero <brayanstewartguor@gmail.com>
2025-09-13 17:18:47 -0500

    fix: Fix open api docs

--- src/main/java/com/test/ecommerceorderservice/configs/OpenApiConfig.java
+++ src/main/java/com/test/ecommerceorderservice/configs/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.test.ecommerceorderservice.infrastructure.security.config;
+package com.test.ecommerceorderservice.configs;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
